4e1446ec139fdb500a6e9967588b46c67a487f81
SoftEngineerGabriel <farias2752@gmail.com>
2023-09-29 05:25:25 -0300

    Aplicação concluida

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ const m = document.querySelector('#m')
 const navHome = document.querySelector('#home')
 const navUniverse = document.querySelector('#universe')
 const navExploration = document.querySelector('#exploration')
+const btn = document.querySelector('button')
+
+
+
 
 handleClickHome.addEventListener('click', ()=>{
-    elApp.classList.add('home')
     m.style.backgroundImage = "url('/assets/mountains-universe-1.png')";
     navHome.style.color = "white"
     navHome.style.fontWeight = "bold";
@@ -16,26 +19,14 @@ handleClickHome.addEventListener('click', ()=>{
     navUniverse.style.fontWeight = "normal"
     navExploration.style.color = "#E1E1E6"
     navExploration.style.fontWeight = "normal"
-
-
 })
 
 
-handleClickUniverse.addEventListener('click', ()=>{
-    elApp.classList.add('secondaryDefault')
-    elApp.classList.remove('home')
-    m.style.backgroundImage = "url('/assets/mountains-universe02.png')";
-    navHome.style.color = "#E1E1E6"
-    navHome.style.fontWeight = "normal"
-    navUniverse.style.color = "white"
-    navUniverse.style.fontWeight = "bold"
-    navExploration.style.color = "#E1E1E6"
-    navExploration.style.fontWeight = "normal"
+handleClickUniverse.addEventListener('click',() =>{
+    universePage()
 })
 
 handleClickExploration.addEventListener('click', ()=>{
-    elApp.classList.add('secondaryDefault')
-    elApp.classList.remove('home')
     m.style.backgroundImage = "url('/assets/mountains-universe-3.png')";
     navHome.style.color = "#E1E1E6"
     navHome.style.fontWeight = "normal"
@@ -61,12 +52,31 @@ function route(event){
 
 function handle(){
     const {pathname} = window.location; //localizar em qual página estamos
-    console.log(pathname);
 
     const route = routes[pathname] || routes[404]; // qualquer endereço fora da aplicação ele irá retonar a página de 404, indicando erro.
 
     fetch(route).then(data => data.text()).then(html => {document.querySelector("#app").innerHTML = html}) //ela irá colocar as páginas html dentro da nossa aplicação (#app) com isso irá mudar ao ser clicado em qualquer link do nav.
 
 }
+function universePage(){
+    m.style.backgroundImage = "url('/assets/mountains-universe02.png')";
+    navHome.style.color = "#E1E1E6"
+    navHome.style.fontWeight = "normal"
+    navUniverse.style.color = "white"
+    navUniverse.style.fontWeight = "bold"
+    navExploration.style.color = "#E1E1E6"
+    navExploration.style.fontWeight = "normal"
+}
+
+function homePage(){
+    
+
+    fetch('/pages/universe.html').then(response => response.text()).then(html => {
+        document.querySelector('#app').innerHTML = html;
+    })
+    universePage()
+}
 
 handle() //essa função inicia executando pois ela chama o home para ser a tela inicial da aplicação
+
+//nav home ao iniciar 
